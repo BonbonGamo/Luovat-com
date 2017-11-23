844f885770d0b40b5236fe9a716fdb3335da44c7
Petteri Ponkamo <petteri.ponkamo@gmail.com>
2017-11-23 21:48:13 +0200

    Frontpage v1 ready

--- public/javascripts/controller/main.js
+++ public/javascripts/controller/main.js
@@ -98,7 +98,7 @@ Vue.component('order-form',{
         },
     },
     template:
-        '<div class="col-md-10 col-md-offset-1">'+
+        '<div class="col-md-10 col-md-offset-1 col-lg-8 col-lg-offset-2">'+
             '<div class="col-sm-6 col-sm-offset-3">'+
                 '<center>'+
                     '<h2 class="opensans">'+
@@ -110,30 +110,30 @@ Vue.component('order-form',{
                 '</center>'+
             '</div>'+
             '<div id="select-size" class="hide-order-form">'+
-                '<div class="col-md-12 text-center m10"><span><a id="play-sample-video" class="btn btn-xs green btn-white">Miten tilaan</a> <a data-toggle="modal" data-target="#compare" class="btn green btn-xs btn-white">Vertaile paketteja</a></span></div>'+
-                '<order-size small="Valitse" caption="SMALL" class="col-md-4" size="s" price="790" description="Pienin pakettimme sisältää muutaman tunnin kuvauksen sekä yhden päivän jälkituotannon. Paketti soveltuu esimerkiksi haastatteluiden taltiontiin."></order-size>'+
-                '<order-size small="Valitse" caption="MEDIUM" class="col-md-4" size="m" price="990" description="Keskikokoinen pakettimme mahdollistaa puolen päivän kuvauksen sekä joustavamman jälkituotannon. Paketti soveltuu esimerkiksi tapahtumien taltiointiin."></order-size>'+
-                '<order-size small="Valitse" caption="LARGE" class="col-md-4" size="l" price="1390" description="Iso pakettimme tarjoaa esituotantoa, kokonaisen kuvauspäivän sekä laajemman jälkituotannon. Kyseinen tuotanto mahdollistaa mm. yritysvideon."></order-size>'+
+                '<div class="col-md-12 text-center m10"><span><a id="play-sample-video" class="btn btn-xs green btn-white">Näin tilaat</a> <a data-toggle="modal" data-target="#compare" class="btn green btn-xs btn-white">Vertaile paketteja</a></span></div>'+
+                '<order-size small="S" caption="SMALL" class="col-md-4" size="s" price="790" description="Pienin pakettimme sisältää muutaman tunnin kuvauksen sekä yhden päivän jälkituotannon. Paketti soveltuu esimerkiksi haastatteluiden taltiontiin."></order-size>'+
+                '<order-size small="M" caption="MEDIUM" class="col-md-4" size="m" price="990" description="Keskikokoinen pakettimme mahdollistaa puolen päivän kuvauksen sekä joustavamman jälkituotannon. Paketti soveltuu esimerkiksi tapahtumien taltiointiin."></order-size>'+
+                '<order-size small="L" caption="LARGE" class="col-md-4" size="l" price="1390" description="Iso pakettimme tarjoaa esituotantoa, kokonaisen kuvauspäivän sekä laajemman jälkituotannon. Kyseinen tuotanto mahdollistaa mm. yritysvideon."></order-size>'+
             '</div>'+
             '<div id="askFirst" class="hide-order-form blue col-sm-6 col-sm-offset-3"style="display:none;">'+
                 '<center>'+
-                    '<h3 class="opensans">1/2 Tilaajan tiedot </h3>'+
+                    '<h3 class="montserrat fw700 white">1/2 Tilaajan tiedot </h3>'+
                 '</center>'+
-                '<input class="form-control m20 order-form-input" placeholder="Nimesi" v-model="formData.name"></input>'+
-                '<input class="form-control m20 order-form-input" placeholder="Sähköpostiosoitteesi" type="email" v-model="formData.email">'+
-                '<textarea rows="10" class="form-control m20 order-form-input-area" placeholder="Kerro meille videotarpeestasi" v-model="formData.message"></textarea>'+
+                '<input class="form-control m20 order-form-input darkblue" placeholder="Nimesi" v-model="formData.name"></input>'+
+                '<input class="form-control m20 order-form-input darkblue" placeholder="Sähköpostiosoitteesi" type="email" v-model="formData.email">'+
+                '<textarea rows="10" class="form-control m20 order-form-input-area darkblue" placeholder="Kerro meille videotarpeestasi" v-model="formData.message"></textarea>'+
                 '<center>'+
                     '<button class="btn btn-sm btn-white" v-on:click="newOrder()">Peruuta</button><button class="btn btn-sm btn-white" v-on:click="askAdditional()">Seuraava</button>'+
                 '</center>'+
             '</div>'+
             '<div id="ask-additional" class="hide-order-form blue col-sm-6 col-sm-offset-3" style="display:none;">'+
                 '<center>'+
-                    '<h3 class="opensans">2/2 Lisätiedot ja tilauksen lähetys </h3>'+
+                    '<h3 class="montserrat fw700 white">2/2 Lisätiedot ja tilauksen lähetys </h3>'+
                 '</center>'+
-                '<input class="form-control m20 order-form-input-area" placeholder="Yritys" v-model="formData.company"></input>'+
-                '<input class="form-control m20 order-form-input-area" placeholder="Puhelin" v-model="formData.phone"></input>'+
-                '<input class="form-control m20 order-form-input-area" placeholder="Kaupunki" v-model="formData.eventCity"></input>'+
-                '<input class="form-control m20 order-form-input-area" placeholder="Päivä" type="date" v-model="formData.date"></input>'+ 
+                '<input class="form-control m20 order-form-input-area darkblue" placeholder="Yritys" v-model="formData.company"></input>'+
+                '<input class="form-control m20 order-form-input-area darkblue" placeholder="Puhelin" v-model="formData.phone"></input>'+
+                '<input class="form-control m20 order-form-input-area darkblue" placeholder="Kaupunki" v-model="formData.eventCity"></input>'+
+                '<input class="form-control m20 order-form-input-area darkblue" placeholder="Päivä" type="date" v-model="formData.date"></input>'+ 
                 '<label class="white m10" for="12" >  Tekstitys +50€</label>'+
                 '<input type="checkbox" id="a1" v-model="formData.add1"><br>'+
                 '<label class="white m10" for="a2" >  Ilmakuvaus +100€</label>'+
@@ -176,15 +176,13 @@ Vue.component('order-size',{
     '<div>'+
         '<center>'+
             '<div class="well-order" v-on:click="openForm(size)">'+
-                '<div v-bind:id="bgId" class="well-order-inner">'+
-                '</div>'+
-                '<div class="well-order-content">'+
-                '<center>'+
-                        //'<p class="white" style="font-size:10px;transform:translateY(0px)">{{ small }}</p>'+
-                        '<h1 style="line-height:50px;" class="giant">{{ size }}</h1>'+     
+                '<div class="">'+
+                '<center>'+                 
+                        '<h1 style="font-size:150px;text-transform:uppercase;" class="darkblue plaster">{{ size }}</h1>'+ 
+                        '<h3 class="blue fw400 style="">Valitse <span class="fw900">{{ small }}</span> paketti</h3>'+   
                         '<p class="black opensans" style="font-size:16px;">{{ description }}</p>'+
                         '<p class="black" style="font-size:10px;transform:translateY(25px)">Hinta alv 0%</p>'+
-                        '<h3 class="black price">{{ price }}€</h3>'+
+                        '<h3 class="blue fw700 price">{{ price }}€</h3>'+
                     '</center>'+
                 '</div>'+
             '</div>'+
