e6f717c40acd5fd63e04c2cb147045f0523c511c
Petteri Ponkamo <petteri.ponkamo@gmail.com>
2017-12-19 19:31:38 +0200

    Luovat_user: ACCESSLEVEL ADDED

--- migrations/20171219192721_luovat_7.js
+++ migrations/20171219192721_luovat_7.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-    return knex.schema.table('luovat_user', function(t){
-        t.string('userLevel')
-    })
+    return Promise.all([
+        knex.schema.table('luovat_user', function(t){
+            t.string('userLevel')
+        }),
+        knex.schema.table('luovat_order', function(t){
+            t.string('invoice20CreatedAt')
+            t.string('invoice100CreatedAt')
+            t.integer('voiceOverPrice')
+        })   
+    ]) 
 };
 
 exports.down = function(knex, Promise) {
