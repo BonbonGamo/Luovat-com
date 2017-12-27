03615bcfa82b831aaf795aae0960cd5e0fbb4f1b
Petteri Ponkamo <petteri.ponkamo@gmail.com>
2017-12-27 15:38:02 +0200

    latest

--- migrations/20171223215149_addname.js
+++ migrations/20171223215149_addname.js
@@ -1,8 +1,9 @@
-
 exports.up = function(knex, Promise) {
+
     return knex.schema.table('luovat_campaign', function(t){
         t.string('campaignName')
-    })
+    });
+    
 };
 
 exports.down = function(knex, Promise) {
