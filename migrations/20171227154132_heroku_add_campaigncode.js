64f9bf77b71be504e777d15639c32314d14913da
Petteri Ponkamo <petteri.ponkamo@gmail.com>
2017-12-27 15:42:19 +0200

    latest

---
+++ migrations/20171227154132_heroku_add_campaigncode.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+
+    return knex.schema.table('luovat_order', function(t){
+        t.string('campaignCode')
+    });
+    
+};
+
+exports.down = function(knex, Promise) {
+    
+};
